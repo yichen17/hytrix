815b7a02e7312f771cda4f990a1d265f79da094d
qiuxinchao <qiuxinchao@fbank.com.cn>
2021-10-29 16:35:59 +0800

    两张表记录，hot_news_back为基础表，hot_news多3个text字段

--- heartbeat/client/src/main/java/client/demo/task/HotNewsTask.java
+++ heartbeat/client/src/main/java/client/demo/task/HotNewsTask.java
@@ -1,12 +1,14 @@
 package client.demo.task;
 
+import client.demo.dao.HotNewsBackMapper;
 import client.demo.dao.HotNewsMapper;
-import client.demo.model.HotNews;
+import client.demo.model.HotNewsBack;
+import client.demo.model.HotNewsWithBLOBs;
+import cn.hutool.extra.mail.MailUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -30,6 +32,9 @@ public class HotNewsTask {
 
     @Autowired
     private HotNewsMapper hotNewsMapper;
+
+    @Autowired
+    private HotNewsBackMapper hotNewsBackMapper;
     /**
      * 访问量 pattern， 获取访问量中的数值
      */
@@ -50,15 +55,16 @@ public class HotNewsTask {
     /**
      *  百度 热门 新闻 记录
      */
-    @Scheduled(cron = "0 0 5,16 * * ?")
+//    @Scheduled(cron = "0 0 5,16 * * ?")
+    @Scheduled(cron = "0 * * * * ?")
     public void loadHotNewsBaidu() {
         try{
-            Document doc = Jsoup.connect(baidu_url).get();
+            Document doc = Jsoup.connect(baidu_url).timeout(2000).get();
             Element hotNews = doc.getElementsByClass("opr-toplist1-table_3K7iH").get(0);
             // 遍历 有2个 一个被隐藏了
-            int total,error;
+            int totalA,errorA,totalB,errorB;
             for(int i=0;i<hotNews.childNodeSize();i++){
-                total=0;error=0;
+                totalA=0;errorA=0;totalB=0;errorB=0;
                 Element child = hotNews.child(i);
                 for(int j=0;j<child.childNodeSize();j++){
                     //  有效数据节点
@@ -71,29 +77,73 @@ public class HotNewsTask {
                     String no=news.child(0).child(0).textNodes().get(0).text();
                     // 具体页面 uri
                     String uri=news.child(0).getElementsByTag("a").attr("href");
-                    HotNews recode=new HotNews();
-                    recode.setChannel("0");
-                    recode.setNo(Integer.parseInt(no));
-                    recode.setUrl("https://www.baidu.com/"+uri);
-                    recode.setTime(new Date());
-                    recode.setTitle(title);
-                    recode.setVisitsReal(visits);
-                    Matcher matcher = VISIT_PATTERN.matcher(visits);
-                    if(matcher.find()){
-                        recode.setVisits(Integer.parseInt(matcher.group()));
-                    }
-                    int res = hotNewsMapper.insert(recode);
-                    if (res == 1) {
-                        total++;
+
+                    HotNewsBack hotNewsBack=constructHotNewsMapper("0",Integer.parseInt(no),
+                            "https://www.baidu.com/"+uri,new Date(),title,visits);
+                    int resA = hotNewsBackMapper.insert(hotNewsBack);
+                    if (resA == 1) {
+                        totalA++;
                     } else {
-                        error++;
+                        errorA++;
                     }
+
+                    HotNewsWithBLOBs hotNewsWithBLOBs=constructHotNewsMapper("0",Integer.parseInt(no),
+                            "https://www.baidu.com/"+uri,new Date(),title,visits,"","","");
+                    int resB = hotNewsMapper.insert(hotNewsWithBLOBs);
+                    if (resB == 1) {
+                        totalB++;
+                    } else {
+                        errorB++;
+                    }
+
                 }
-                System.out.println("total "+total+"  error"+error);
+                log.info("totalA {},errorA {}, totalB {}, errorB {}",totalA,errorA,totalB,errorB);
+                MailUtil.send("q07218396@163.com", "每天百度热门信息记录",
+                        "扩展表 => success "+totalA+" error "+errorA+". 一般表 => success "+totalB+" error "+errorB, false);
             }
         } catch (IOException e) {
             log.warn("jsoup 获取数据出错");
         }
+    }
+
+    /**
+     *  构造扩展数据
+     */
+    public HotNewsWithBLOBs constructHotNewsMapper(String channel, Integer no, String url, Date date, String title,
+                                                   String visits,String imageUrl,String extra1,String extra2){
+        HotNewsWithBLOBs hotNewsWithBlobs=new HotNewsWithBLOBs();
+        hotNewsWithBlobs.setChannel(channel);
+        hotNewsWithBlobs.setNo(no);
+        hotNewsWithBlobs.setUrl(url);
+        hotNewsWithBlobs.setTime(date);
+        hotNewsWithBlobs.setTitle(title);
+        hotNewsWithBlobs.setVisitsReal(visits);
+        hotNewsWithBlobs.setImageurl(imageUrl);
+        hotNewsWithBlobs.setExtra1(extra1);
+        hotNewsWithBlobs.setExtra2(extra2);
+        Matcher matcher = VISIT_PATTERN.matcher(visits);
+        if(matcher.find()){
+            hotNewsWithBlobs.setVisits(Integer.parseInt(matcher.group()));
+        }
+        return hotNewsWithBlobs;
+    }
+
+    /**
+     * 构造一般消息
+     */
+    public HotNewsBack constructHotNewsMapper(String channel,Integer no,String url,Date date,String title,String visits){
+        HotNewsBack hotNewsBack=new HotNewsBack();
+        hotNewsBack.setChannel(channel);
+        hotNewsBack.setNo(no);
+        hotNewsBack.setUrl(url);
+        hotNewsBack.setTime(date);
+        hotNewsBack.setTitle(title);
+        hotNewsBack.setVisitsReal(visits);
+        Matcher matcher = VISIT_PATTERN.matcher(visits);
+        if(matcher.find()){
+            hotNewsBack.setVisits(Integer.parseInt(matcher.group()));
+        }
+        return hotNewsBack;
     }
 
     /**
