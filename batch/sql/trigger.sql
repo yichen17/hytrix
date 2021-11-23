38b17b7249a442e8455168a7e5d91f537fcfdf61
qiuxinchao <qiuxinchao@fbank.com.cn>
2021-11-23 17:10:08 +0800

    trigger sql

---
+++ batch/sql/trigger.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `t_trigger_analyse` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `amount` decimal(16,2) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1452 DEFAULT CHARSET=utf8;
