72a7b9dfd11799303f03c33e4d0b6d91c46af784
qiuxinchao <qiuxinchao@fbank.com.cn>
2021-11-30 09:54:29 +0800

    异步执行逻辑改为同步执行，以捕获执行结果。通过Thread.join()和Thread.sleep()实现。线程切换原理和时间间隔待研究

---
+++ case-test/src/main/java/com/yichen/casetest/test/Test.java
@@ -0,0 +1,13 @@
+package com.yichen.casetest.test;
+
+/**
+ * @author Qiuxinchao
+ * @version 1.0
+ * @date 2021/11/15 18:03
+ * @describe
+ */
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
